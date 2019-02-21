b73c25e544b319670af9b8b4e4a9a173f6cfb46c
Caian R. Ertl <caianrais@protonmail.com>
2019-02-21 01:22:18 -0300

    feat: add the "Exchanger" module (not-yet-implemented)

---
+++ src/exchanger.cr
@@ -0,0 +1,10 @@
+module Exchanger
+    class Fixer
+        @amount = Int32
+        @origin = String
+        @targets = [] of String
+
+        def initialize(amount, origin, targets)
+        end
+    end
+end
