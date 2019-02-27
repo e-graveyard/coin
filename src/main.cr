161cbdfed52ef0dbd54c31fe4f9496ee6672caa4
Caian R. Ertl <caianrais@protonmail.com>
2019-02-27 00:03:48 -0300

    refact(main): treat the exceptions inside the main routine

--- src/main.cr
+++ src/main.cr
@@ -3,14 +3,28 @@ require "./exchanger"
 
 
 def main
-    parser = CLI::Parser.new(ARGV)
-    amount, origin, targets = parser.act
+    parser = CLI::Parser.new ARGV
+
+    begin
+        amount, origin, targets = parser.act
+
+    rescue e : CLI::Exception
+        CLI::Dialog.die "#{e.message}"
+    end
 
     token = (t = ENV["FIXER_API_TOKEN"]?) ? t : ""
 
-    fixer = Exchanger::Fixer.new(amount, origin, targets)
-    fixer.set_token(token)
-    fixer.convert
+    fixer = Exchanger::Fixer.new amount, origin, targets
+    fixer.set_token token
+
+    begin
+        results = fixer.convert
+
+    rescue e : Exchanger::Exception
+        CLI::Dialog.die "#{e.message}"
+    end
+
+    puts results
 end
 
 main
