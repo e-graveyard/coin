d02756c8836a3fd1aeb7a8a6612074c36054ebee
Caian R. Ertl <hi@caian.org>
2021-04-17 21:09:49 -0300

    ci: crystall instalation fixes

--- .github/install-crystal.sh
+++ .github/install-crystal.sh
@@ -4,11 +4,20 @@ set -e
 
 case "$OS" in
     macos-latest)
+        brew update
+        brew install openssl
+        export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/opt/openssl/lib/pkgconfig
+
         brew install crystal
         ;;
 
     ubuntu-latest)
-        curl -fsSL https://crystal-lang.org/install.sh | sudo bash
+        echo "deb https://dl.bintray.com/crystal/deb all {CHANNEL}" | tee /etc/apt/sources.list.d/crystal.list
+
+        apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+        apt-get update
+
+        sudo apt-get install -y libyaml-dev crystal
         ;;
 esac
 
